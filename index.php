c4cac4240e1c21221cdb319ad66406294d2b5f2c
stefanopriolo <stefanojpriolo@gmail.com>
2023-09-07 15:37:44 +0200

    array print

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center">Hotel</h1>
+        <?php foreach ($hotels as $key => $hotel) { ?>
+            <div class="row p-5">
+                <h3>Nome: <?php echo $hotel['name']; ?> </h3>
+                <h3>Descrizione: <?php echo $hotel['description']; ?> </h3>
+                <h3>Parcheggio: <?php echo $hotel['parking']; ?> </h3>
+                <h3>Voto: <?php echo $hotel['vote']; ?> </h3>
+                <h3>Distanza dal centro: <?php echo $hotel['distance_to_center']; ?> </h3>
+            </div>
+
+        <?php } ?>
+    </div>
+</body>
+
+</html>
